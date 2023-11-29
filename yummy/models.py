d9e648905ee03b65b0b95bb7bab05d1d6d44e21c
OST <oleh.s.tymchuk@gmail.com>
2023-11-29 20:11:06 +0100

    29/11/2023

---
+++ yummy/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+
+class DishCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    order = models.PositiveSmallIntegerField()
+    is_visible = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Dish Categories'
+        ordering = ('order',)
+
+    def __iter__(self):
+        dishes = self.dishes.filter(is_visible=True)
+        for dish in dishes:
+            yield dish
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='url')
+    ingredients = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    weight = models.PositiveSmallIntegerField(blank=True)
+    photo = models.ImageField(upload_to='dishes/', blank=True)
+    is_visible = models.BooleanField(default=True)
+    order = models.PositiveSmallIntegerField()
+
+    category = models.ForeignKey(DishCategory, on_delete=models.PROTECT, related_name='dishes')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+        ordering = ('order',)
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'category'], name='unique_order_per_each_category'),
+        ]
+        unique_together = ['id', 'slug']
+
+
+class Gallery(models.Model):
+    photo = models.ImageField(upload_to='gallery/')
+    is_visible = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, blank=True)
+
+
+
+
+
+
+
+
